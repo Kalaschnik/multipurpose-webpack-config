c9040c64012c3f86365c2927881a2f40b5d5b80b
Steven Kalinke <9831678+Kalaschnik@users.noreply.github.com>
2021-04-15 16:19:02 +0200

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
+
+module.exports = {
+  mode: mode, // default: production, other: development
+
+  // entry: "./src/index.js", // default: ./src/index.js
+  // output: {
+  //   filename: "bundle.js", // default: main.js
+  //   path: path.resolve(__dirname, "public"), // <- default: dist
+  // },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          // without additional settings, this will refer to .babelrc
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public/", to: "./" }],
+    }),
+  ],
+
+  devtool: "source-map",
+
+  devServer: {
+    contentBase: "./dist",
+  },
+};
